3884bb37edabcea5b32a28432794cbbe25f26f1f
Joshua <joshua@perivi.com>
2016-03-12 17:43:36 -0500

    Simplify SinglePossibilityStrategy and OnlyChoiceStrategy
    This works now that the Grid updates the possibilities when a Cell's
    value is set.

--- src/com/perivi/sudoku/java/OnlyChoiceStrategy.java
+++ src/com/perivi/sudoku/java/OnlyChoiceStrategy.java
@@ -1,49 +1,17 @@
 package com.perivi.sudoku.java;
 
-import java.util.List;
-import java.util.Set;
-
-import com.google.common.collect.Sets;
-import com.perivi.sudoku.java.Grid.CellState;
-
 /**
  * If there is only one remaining possibility in a row or column, it must fill
  * the remaining cell.
+ *
+ * This actually does nothing since the SinglePossibilityStrategy supersedes it.
+ *
  * @author jdh
  *
  */
 public class OnlyChoiceStrategy implements Strategy {
-    @Override
-    public Boolean apply(final Grid grid) {
-        for (final Iterable<Grid.Cell> section : grid.sections()) {
-            final List<Integer> values = Grid.values(section);
-            final Set<Integer> valueSet = Sets.newHashSet(values);
-            final Set<Integer> remainingPossibilities =
-                    Sets.difference(Grid.ALL_POSSIBILITIES, valueSet);
-
-            // Update notes.
-//            for (final Grid.Cell c : section) {
-//            	for (final Integer p : c.getNotes()) {
-//            		if (!remainingPossibilities.contains(p)) {
-//            			c.removeNote(p);
-//            		}
-//            	}
-//            }
-
-            if (remainingPossibilities.size() == 1) {
-                final Integer lastPossibility = remainingPossibilities.iterator().next();
-                for (final Grid.Cell c : section) {
-                    if (c.getValue() == null) {
-//                        System.out.println(String.format("Section %s seems like only possibility for this cell is: %s",
-//                                section, lastPossibility));
-                        c.setValue(lastPossibility);
-                        c.setState(CellState.HINT);
-                        return Boolean.TRUE;
-                    }
-                }
-            }
-        }
-
-        return Boolean.FALSE;
-    }
+	@Override
+	public Boolean apply(final Grid grid) {
+		return Boolean.FALSE;
+	}
 }
--- src/com/perivi/sudoku/java/SinglePossibilityStrategy.java
+++ src/com/perivi/sudoku/java/SinglePossibilityStrategy.java
@@ -1,10 +1,6 @@
 package com.perivi.sudoku.java;
 
-import java.util.Set;
-
-import com.google.common.collect.Sets;
 import com.perivi.sudoku.java.Grid.CellState;
-import com.perivi.sudoku.java.Grid.House;
 
 /**
  * If between the row, column, and house, a possibility is the only remaining
@@ -16,37 +12,16 @@ import com.perivi.sudoku.java.Grid.House;
 public class SinglePossibilityStrategy implements Strategy {
 
     @Override
-    public Boolean apply(final Grid input) {
-        for (int i = 0; i < input.getHeight(); ++i) {
-            for (int j = 0; j < input.getWidth(); ++j) {
-                if (input.cellAt(i, j).getValue() != null) {
-                    continue;
-                }
+    public Boolean apply(final Grid grid) {
+    	for (final Grid.Cell cell : grid.cells()) {
+    		if (cell.getNotes().size() == 1 && cell.getValue() == null) {
+    			cell.setValue(cell.getNotes().iterator().next());
+    			cell.setState(CellState.HINT);
+    			return Boolean.TRUE;
+    		}
+    	}
 
-                final Set<Integer> rowValues = Sets.newHashSet(Grid.values(input.row(i)));
-                final Set<Integer> colValues = Sets.newHashSet(Grid.values(input.column(j)));
-                final House houseContaining = input.houseContaining(i, j);
-                final Set<Integer> houseValues = Sets.newHashSet(Grid.values(houseContaining));
-
-                final Set<Integer> remainingPossibilities = Sets.difference(Grid.ALL_POSSIBILITIES,
-                        Sets.union(rowValues, Sets.union(colValues, houseValues)));
-
-                if (remainingPossibilities.size() == 1) {
-//                    System.out.println(String.format("cell(%d,%d): rowValues %s, colValues %s, %s houseValues %s: remainingPossibilities %s",
-//                            i, j,
-//                            rowValues,
-//                            colValues,
-//                            houseContaining,
-//                            houseValues,
-//                            remainingPossibilities));
-                    input.cellAt(i, j).setValue(remainingPossibilities.iterator().next());
-                    input.cellAt(i, j).setState(CellState.HINT);
-                    return Boolean.TRUE;
-                }
-            }
-        }
-
-        return Boolean.FALSE;
+    	return Boolean.FALSE;
     }
 
 }
